7f1d9fcb377d61c454e7953f00d71f1bb459ca6e
“Sara” <Sara.CobaledaC@gmail.com>
2023-10-02 22:15:48 -0500

    consultar la API de sismos

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent, LoadingController } from '@ionic/angular';
+import { SismosService } from 'src/app/services/sismos.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor() {}
+  /*
+  sismos = [];
+
+  constructor(
+    private sismosService: SismosService,
+    private loadingCtrl: LoadingController
+  ) {}
+  */
+
+  earthquakes: any;
+
+  constructor(private earthquakeService: SismosService) { }
+
+  ngOnInit(): void {
+    this.earthquakeService.getEarthquakes().subscribe(data => {
+      this.earthquakes = data;
+      console.log('Datos de terremotos:', this.earthquakes);
+    });
+  }
+  
+  /*
+  ngOnInit() {
+    this.loadSismos();
+  }
+  async loadSismos(event?: InfiniteScrollCustomEvent) {
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading..',
+      spinner: 'bubbles',
+    });
+    await loading.present();
+
+    this.sismosService.getCatalogs().subscribe(
+      (res) => {
+        loading.dismiss();
+        this.sismos.push(...res.results);
+
+        event?.target.complete();
+
+      },
+      (err) => {
+        console.log(err);
+        loading.dismiss();
+      }
+    );
+  }
+
+  loadMore(event: InfiniteScrollCustomEvent) {
+    this.loadSismos(event);
+  }
+  */
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02',
 };
 
 /*
